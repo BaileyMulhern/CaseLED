6c6b173365a1c4a4452ec7e9be9226cea7a6ddb1
Bailey Mulhern <mulhern.15@osu.edu>
2021-01-21 16:58:05 -0500

    Created demo reel using new effects

--- CaseLED.ino
+++ CaseLED.ino
@@ -1,42 +1,28 @@
 //Include needed Libraries at beginning
 #include <Arduino.h>
 #include <FastLED.h> // FastLED library for WS2812 RGB Stick http://fastled.io/
-#include "src/LedUtilities/ScriptManager.h"
+#include "src/LedUtilities/LedUtilities.h"
 
 
 // Interrupts on 2, 3
 // PWM on 3, 5, 6, 9, 10, 11
 #define LED_PIN         2
 #define BRIGHTNESS      255
+#define HUE_MAX			255
 #define NUM_LEDS        72
 #define LOOP_DELAY      1
 
-CRGB leds[NUM_LEDS];
+CRGBArray<NUM_LEDS> leds;
+Strip strip(&leds, false, true, true);
+EffectManager effect_manager(&strip);
+ScriptManager script_manager(&strip);
 
-ScriptManager::ScriptElement script[] = {
-
-	{EffectManager::FIRE, CRGB::Red, EffectManager::MEDIUM, 0, 0, 1},
-
-    {EffectManager::SMOOTH_CHASE, CRGB::Cyan, EffectManager::MEDIUM, 0, 5, 0},
-    {EffectManager::SMOOTH_CHASE_2, CRGB::Cyan, EffectManager::FAST, 0, 5, 0},
-    {EffectManager::SMOOTH_CHASE_3, CRGB::Cyan, EffectManager::VERY_FAST, 0, 5, 0},
-    {EffectManager::SMOOTH_CHASE_4, CRGB::Cyan, EffectManager::VERY_FAST, 0, 5, 0},
-    
-    {EffectManager::THEATRE_CHASE, CRGB::Blue, EffectManager::MEDIUM, 0, 5, 0},
-    {EffectManager::THEATRE_CHASE_2, CRGB::Blue, EffectManager::FAST, 0, 5, 0},
-    {EffectManager::THEATRE_CHASE_3, CRGB::Blue, EffectManager::FAST, 0, 5, 0},
-    {EffectManager::THEATRE_CHASE_4, CRGB::Blue, EffectManager::FAST, 0, 5, 0},
-       
-    {EffectManager::THEATRE_CHASE, CRGB::Red, EffectManager::VERY_SLOW, 0, 5, 0},
-    {EffectManager::THEATRE_CHASE, CRGB::Green, EffectManager::SLOW, 0, 5, 0},
-    
-    {EffectManager::RAINBOW_EVEN, CRGB::Red, EffectManager::MEDIUM, 0, 10, 0},
-    {EffectManager::RAINBOW_FADE, CRGB::Red, EffectManager::VERY_FAST, 0, 10, 0},
-
-    
+ScriptElement script[] = {
+	{WIPE, (5 * Timer::ONE_SECOND), false},
+	{CONFETTI, (5 * Timer::ONE_SECOND), false},
+	{FADE, (5 * Timer::ONE_SECOND), false},
+	{FIRE, (5 * Timer::ONE_SECOND), false},
 };
-
-ScriptManager scriptManager(leds, NUM_LEDS);
 
 uint8_t length = sizeof(script) / sizeof(script[0]);
 
@@ -44,18 +30,17 @@ void setup(void)
 {
     pinMode(LED_PIN, OUTPUT);
 
-    FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
+    FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, leds.size());
 
     FastLED.setBrightness(BRIGHTNESS);
 
-    scriptManager.loadScript(script, length);
-    
+    script_manager.loadScript(script, length);
     Serial.begin(115200);
 }
 
 void loop(void)
 {
-    scriptManager.runScript();
+    script_manager.runScript();
 
     FastLED.show();
 }
